f5f2e17ddb246b0d6b8858c8d928a4e5e9353752
Dan Electronics <SimpleElectronicsYT@gmail.com>
2026-02-28 09:48:33 -0500

    added some DRY principles and made it nicer for the user

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 from pokemon import Pokemon
 
 def main():
@@ -19,14 +20,32 @@ def main():
         
     # annouce who won the coin toss
     print(f"{first.name} won the coin toss! It will go first!")
+    print("")
+    round = 1
+    
     
     # ensure that both pokemon are healthy and do the game loop
     while charmander.hp > 0 and squirtle.hp > 0:
+        
+        print(f"Round {round}!")
+        print("")
         
         # ensure that the first attacker has enough hp to attack
         if first.hp > 0:
             first.attack(second)
             print(f"{second.name} has {second.hp} hp remaining!")
+        
+        if first.hp <= 0:
+            print(f"{first.name} has fainted!")
+            break
+            
+        if second.hp <= 0:
+            print(f"{second.name} has fainted!")
+            break
+        
+        print(" ")
+        # pause a bit to let the 'player' read
+        time.sleep(5)
         
         # ensure that the second attacker has enough hp to attack
         if second.hp > 0:
@@ -41,6 +60,13 @@ def main():
         if second.hp <= 0:
             print(f"{second.name} has fainted!")
             break
+        
+        print("-------------------------")
+        # pause a bit to let the 'player' read
+        time.sleep(5)
+        
+        round += 1
+        
     
     quit()
 
--- pokemon.py
+++ pokemon.py
@@ -1,3 +1,5 @@
+import random
+
 class Pokemon:
     def __init__(self, name, hp, type, weakness, resistance):
         self.name = name
@@ -10,27 +12,30 @@ class Pokemon:
         target.take_damage(self, 10)
         
     def take_damage(self, attacker, damage):
+        
+        print(f"{attacker.name} uses 'Attack' on {self.name}!")
+        
         total_damage = 0
         
         # check for weakness
         if attacker.type == self.weakness:
             total_damage = round(damage * 2, -1)
-            self.hp -= total_damage
             print(f"{attacker.name}'s attack is super effective!")
-            print(f"{attacker.name} hits {self.name} for {total_damage}!")
-        
+            
         # check for resistance    
         elif attacker.type == self.resistance:
             total_damage = max(0, damage - 30)
-            self.hp -= total_damage
             print(f"{attacker.name}'s attack is resisted by {self.name}!")
-            print(f"{attacker.name} hits {self.name} for {total_damage}!")
         
         # just do regular damage if not    
         else:
             total_damage = damage
-            self.hp -= total_damage
-            print(f"{attacker.name} hits {self.name} for {total_damage}!")
             
+        self.hp -= total_damage
+        print(f"{attacker.name} hits {self.name} for {total_damage}!")    
+        
         if self.hp < 0:
             self.hp = 0
+            
+    def flip_coin(self):
+        return random.choice(["heads", "tails"])
