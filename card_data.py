cc3196295ae5e22d369378698633823f581c8c44
Dan Electronics <SimpleElectronicsYT@gmail.com>
2026-03-01 08:24:30 -0500

    added dictionary file

---
+++ card_data.py
@@ -0,0 +1,25 @@
+charmander_46 = {
+    "name": "Charmander",
+    "hp": 50,
+    "type": "fire",
+    "stage": 1,
+    "weakness": "water",
+    "resistance": "no_type",
+    "retreat": 1,
+    "evolves_from": "none",
+    "pkmn_power": "none",
+    "attack_one": {
+        "name": "Scratch",
+        "damage": 10,
+        "energy_amount_any": 1,
+        "energy_amount_specific": 0,
+        "specific_energy_type": "fire"
+    },
+    "attack_two": {
+        "name": "Ember",
+        "damage": 30,
+        "energy_amount_any": 1,
+        "energy_amount_specific": 1,
+        "specific_energy_type": "fire"
+    }
+}
